51a4261e2d4b3e712ddc0181b341f9180bcd15a3
ShawnXiha <huakaijianwo2009@gmail.com>
2017-08-12 10:40:32 +0800

    补充向量的内积，向量间角度，投影等方法

--- vector.py
+++ vector.py
@@ -39,9 +39,32 @@ class Vector:
     def direction(self):
         l = self.magitude()
         return Vector([i/l for i in self.coordinates])
+    def dotProduct(self, other):
+        assert isinstance(other, Vector)
+        assert self.dimension == other.dimension
+        return sum([i*j for i,j in zip(self.coordinates, other.coordinates)])
 
+    def direction_between(self, other):
+        return math.acos(self.dotProduct(other)/(self.magitude()*other.magitude()))
 
+    def parallel(self, other,tolerance=1e-10):
+        assert isinstance(other, Vector)
+        assert self.dimension == other.dimension
+        bt = self.direction_between(other)
+        negbt = abs(math.pi - bt)
+        if bt < tolerance or negbt < tolerance:
+            return True
+        else:
+            return False
 
-myvector = Vector([3,4])
+    def isOrthogonal(self,v,tolerance=1e-10):
+        if abs(self.dotProduct(v)) < tolerance:
+            return True
+        else:
+            return False
+    def prejection(self, other):
+        return self.dotProduct(other)*other.direction()
 
-print(myvector.direction())
+myvector = Vector([1,0])
+myvector2 = Vector([0,-4])
+print(myvector.prejection(myvector2))
